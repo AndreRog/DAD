6b4ffd39ffe097baf73c78e8a55baa8d642b1a2b
andresantos_ <andre.rog.santos@gmail.com>
2015-10-26 15:32:27 +0000

    SESDAD creation

---
+++ SESDAD/Broker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
